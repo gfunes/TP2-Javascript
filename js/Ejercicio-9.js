85b0b2d4197db06db5c882fd690c6d6da45251fd
Gabriel Funes <gfunes@gmail.com>
2026-03-13 18:08:20 -0300

    resuelto  en parte, falta la linea cada 5 nros

--- js/Ejercicio-9.js
+++ js/Ejercicio-9.js
@@ -1,17 +1,28 @@
 //Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9
 // y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
 const pantalla = document.getElementById("pantalla");
+const asteriscos = document.getElementById("asteriscos");
 let i = 1;
-while (i <= 5) {
-  
-    console.log(i);
-    if( i % 2){
-        pantalla.innerHTML = `<p>mensaje ${i}<\p>` 
-    }else{
-      pantalla.innerHTML +=`<p>${i}<p/>` ;  
+let v = 1;
+let h = 5;
+while (i <= 30) {
+  if (v==h){
+      h=h+5;
+     asteriscos.innerHTML = `****************`;
+  }else {
+    if (i % 4 == 0) {
+    pantalla.innerHTML += `<p>${i} es multiplo de 4<\p>`;
+  } else {
+    if (i % 9 == 0) {
+      pantalla.innerHTML += `<p>${i} es multiplo de 9<\p>`;
+    } else {
     }
-  
-  i++;
-    
+    pantalla.innerHTML += `<p>${i}<p/>`;
+  }
 }
-console.log("sali")
+  i++;
+  v++;
+  
+}
+
+console.log(i);
